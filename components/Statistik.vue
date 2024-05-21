2b5018915c136b11008485ac93e1d8cbdfe34e42
syaikhasyuhda <141708479+syaikhasyuhda@users.noreply.github.com>
2024-05-21 12:42:33 +0700

    Add files via upload

--- components/Statistik.vue
+++ components/Statistik.vue
@@ -20,9 +20,16 @@ const data = {
   labels: labels,
   datasets: [{
     label: 'My First dataset',
-    backgroundColor: 'rgb(255, 99, 132)',
+    backgroundColor: [
+    'rgb(202, 166, 219)',
+    'rgb(196, 138, 223)',
+    'rgb(175, 140, 192)',
+    'rgb(175, 135, 193)',
+    'rgb(209, 149, 237)',
+    'rgb(202, 166, 219)',
+    ],
     borderColor: 'rgb(255, 99, 132)',
-    data: [0, 10, 5, 2, 20, 30, 45],
+    data: [20, 10, 10, 15, 20, 30, 45],
   }]
 };
 
